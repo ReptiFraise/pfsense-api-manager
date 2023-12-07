c7e37749118d1bcae22f295611ee5685508eeef9
Alban Calvo <alban@vizyon.ai>
2023-12-07 16:24:54 +0100

    supp

--- src/pfsense_manager/main.py
+++ src/pfsense_manager/main.py
@@ -9,19 +9,9 @@ def callback():
     pfSense API management tool
     """
 
-@app.command()
-def add_address(ip):
-    """
-    Add an ip address or a list of ip addresses, separate addresses with comma.
-    """
-    if ip.count(",") >= 1:
-        typer.echo(f"IP addresses {ip} added")
-    else:
-        typer.echo(f"IP address {ip} added")
-
 
 @app.command()
-def read_vpn_logs():
+def show_logs(host, user, password):
     """
     Read the vpn logs and create a file to be friendly readable
     """
